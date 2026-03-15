d2f98fc47d81da239fc2ddfe51455201f6f4dcf3
merrygrant-rgb <merry.grant@gmail.com>
2026-03-15 16:54:42 -0500

    Add passthrough copy for chronicles content

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("admin");
 eleventyConfig.addPassthroughCopy({"content/chronicles": "chronicles"});
+
 ```
 
 
