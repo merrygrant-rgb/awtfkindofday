8f706f705d07d90922298345f09eb9f77f8ad53a
merrygrant-rgb <merry.grant@gmail.com>
2026-03-29 14:42:18 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -9,7 +9,7 @@ module.exports = function(eleventyConfig) {
   
 eleventyConfig.addPassthroughCopy({"content/chronicles/episode1-bill-reply-all.html": "chronicles/episode1-bill-reply-all.html"});
 eleventyConfig.addPassthroughCopy({"content/chronicles/episode2-bill-schedule-meeting.html": "chronicles/episode2-bill-schedule-meeting.html"});
-
+eleventyConfig.addPassthroughCopy({"content/chronicles/episode3-bill-quick-call.html": "chronicles/episode3-bill-quick-call.html"});
 
 
 
